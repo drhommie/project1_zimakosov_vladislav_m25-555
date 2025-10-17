f22f4d200c94fb41e4c9146408445c1d7f28084d
Vlad Zimakosov <zimakosov-vladik@yandex.ru>
2025-10-17 22:39:19 +0300

    Part 2.1: Added game data structure and room map

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+from labyrinth_game import player_actions, utils
+
+from labyrinth_game.constants import ROOMS
+
+game_state = {
+    'player_inventory': [],  # Инвентарь игрока
+    'current_room': 'entrance',  # Текущая комната
+    'game_over': False,  # Значения окончания игры
+    'steps_taken': 0  # Количество шагов
+}
+
 def main():
     print("Первая попытка запустить проект!")
 
